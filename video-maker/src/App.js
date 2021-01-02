57911cd4a4b59e53c1551dab7ad62bc25e0012c2
Devarsh Panchal <devarshpanchal@rediffmail.com>
2021-01-02 23:45:16 +0530

    Added Video Background

--- video-maker/src/App.js
+++ video-maker/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="text">
+        <h1>Lets Rewind</h1>
+      </div>
+      <video autoPlay muted loop id="myVideo">
+        <source src="./assets/video.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
