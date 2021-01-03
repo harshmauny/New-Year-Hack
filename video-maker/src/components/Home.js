b939e88d1f2e2d5fe3942aa482dbde7c86a31223
Devarsh Panchal <devarshpanchal@rediffmail.com>
2021-01-03 11:49:01 +0530

    Home Page Design Done # Typo fixed

--- video-maker/src/components/Home.js
+++ video-maker/src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import '../App.css';
 
 var FontAwesome = require('react-fontawesome');
 
